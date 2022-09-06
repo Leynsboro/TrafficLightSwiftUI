d8ae34cb3aad8f9cce7a82ce4fff4d5c87d3a10d
Ilya <leynsboro@gmail.com>
2022-09-06 20:41:07 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Илья Гусаров on 06.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
